4c7d8cc2280f15dfe3d1885eb21883767d3f8e6b
Pedro Arruda <arruda.pedro15@outlook.com>
2024-11-27 14:27:10 -0300

    fix: webhook

--- src/http/controllers/webhook/webhook.ts
+++ src/http/controllers/webhook/webhook.ts
@@ -21,7 +21,7 @@ export const handleWebhook = async (
     });
 
     const handleWebhookEventUseCase = new HandleWebhookEventUseCase();
-    handleWebhookEventUseCase
+    await handleWebhookEventUseCase
       .execute({
         message: Body,
         userId: user.id,
