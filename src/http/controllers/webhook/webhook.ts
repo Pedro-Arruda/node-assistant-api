75191797f04a265a42680bed5e0cd9b3db55ccf8
Pedro Arruda <arruda.pedro15@outlook.com>
2024-12-05 15:22:27 -0300

    fix: api whatsapp

--- src/http/controllers/webhook/webhook.ts
+++ src/http/controllers/webhook/webhook.ts
@@ -3,12 +3,34 @@ import { HandleWebhookEventUseCase } from "../../../use-cases/webhook/handle-web
 import { z } from "zod";
 import { prisma } from "../../../lib/prisma";
 
-export const handleWebhook = async (
-  req: FastifyRequest,
-  reply: FastifyReply
-) => {
-  const body = req.query;
-  console.log("body", body);
+export const handleWebhook = async (req: any, reply: FastifyReply) => {
+  console.log("WEBHOOK");
+
+  const sender = req.body.entry[0].changes[0].value.messages[0].from;
+  const url = `https://graph.facebook.com/v21.0/485600487973744/messages`;
+
+  const data = {
+    messaging_product: "whatsapp",
+    to: sender,
+    text: { body: "TESTEEEEE" },
+  };
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer EAAIeVdZAIBC4BO5R4ALgQ32OxWHgaEYkZC0gdz7Wy7S9TMAkOhg8TimKlAN8ZB8ST92Olj6t6IArCvISQYg0FKr7Liu7VOBjnwPbfWEjnJokv0XmTqUvI0Ngd73gdSjXSyoTC9f5Sxy0iwH4llk2iI0ywgzzWqswjH43jK14TAycZCVwmVXraeV1Q0MNWfZAuPOAgFNDT9koSbLOy1SUedj9oZAKMZD`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => {
+      console.log("Mensagem enviada com sucesso:", response);
+    })
+    .catch((error) => {
+      console.error("Erro ao enviar mensagem:", error.response.data);
+    });
+
+  console.log(response);
 
   // try {
   //   const addSerieBodySchema = z.object({
