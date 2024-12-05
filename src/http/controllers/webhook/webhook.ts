72d9ae841b1cff9944b449c5e07bb0414295bf8c
Pedro Arruda <arruda.pedro15@outlook.com>
2024-12-05 16:03:42 -0300

    feat: add buttons to webhook

--- src/http/controllers/webhook/webhook.ts
+++ src/http/controllers/webhook/webhook.ts
@@ -29,75 +29,67 @@ async function sendMessage(to: number, message: string) {
 }
 
 export const handleWebhook = async (req: any, reply: FastifyReply) => {
-  console.log("WEBHOOK");
+  const url = `https://graph.facebook.com/v21.0/485600487973744/messages`;
 
   const sender = req.body.entry[0].changes[0].value.messages[0].from;
 
   const messageText = req.body.entry[0].changes[0].value.messages[0].text.body;
 
-  await sendMessage(
-    sender,
-    "Obrigado pela sua mensagem! Estamos processando sua solicitação."
-  );
+  const messagePayload = {
+    messaging_product: "whatsapp",
+    to: sender,
+    type: "interactive",
+    interactive: {
+      type: "button",
+      header: {
+        type: "text",
+        text: "Escolha uma opção",
+      },
+      body: {
+        text: "Por favor, selecione uma opção abaixo.",
+      },
+      action: {
+        buttons: [
+          {
+            type: "reply",
+            reply: {
+              id: "series",
+              title: "Séries",
+            },
+          },
+          {
+            type: "reply",
+            reply: {
+              id: "movies",
+              title: "Filmes",
+            },
+          },
+          {
+            type: "reply",
+            reply: {
+              id: "tasks",
+              title: "Tarefas",
+            },
+          },
+          {
+            type: "reply",
+            reply: {
+              id: "compromissos",
+              title: "Compromissos",
+            },
+          },
+        ],
+      },
+    },
+  };
 
-  // const url = `https://graph.facebook.com/v21.0/485600487973744/messages`;
-
-  // const data = {
-  //   messaging_product: "whatsapp",
-  //   to: "5514998861503",
-  //   text: { body: "TESTEEEEE" },
-  // };
-
-  // const response = await fetch(url, {
-  //   method: "POST",
-  //   headers: {
-  //     Authorization: `Bearer EAAIeVdZAIBC4BO5R4ALgQ32OxWHgaEYkZC0gdz7Wy7S9TMAkOhg8TimKlAN8ZB8ST92Olj6t6IArCvISQYg0FKr7Liu7VOBjnwPbfWEjnJokv0XmTqUvI0Ngd73gdSjXSyoTC9f5Sxy0iwH4llk2iI0ywgzzWqswjH43jK14TAycZCVwmVXraeV1Q0MNWfZAuPOAgFNDT9koSbLOy1SUedj9oZAKMZD`,
-  //     "Content-Type": "application/json",
-  //   },
-  //   body: JSON.stringify(data),
-  // })
-  //   .then((response) => {
-  //     console.log("Mensagem enviada com sucesso:", response);
-  //   })
-  //   .catch((error) => {
-  //     console.error("Erro ao enviar mensagem:", error.response.data);
-  //   });
-
-  // try {
-  //   const addSerieBodySchema = z.object({
-  //     Body: z.string(),
-  //     From: z.string(),
-  //   });
-
-  //   const { Body, From } = addSerieBodySchema.parse(req.body);
-
-  //   const user = await prisma.user.findFirstOrThrow({
-  //     where: { phone: From.replace(/\D/g, "") },
-  //     select: { id: true },
-  //   });
-
-  //   const handleWebhookEventUseCase = new HandleWebhookEventUseCase();
-  //   await handleWebhookEventUseCase
-  //     .execute({
-  //       message: Body,
-  //       userId: user.id,
-  //     })
-  //     .catch((error) => {
-  //       console.error("Erro ao processar o evento do webhook:", error);
-  //     });
-
-  //   reply.type("text/xml").status(200).send(`
-  //       <Response>
-  //         <Message>Sua série está sendo inserida.</Message>
-  //       </Response>
-  //     `);
-  // } catch (error: any) {
-  //   console.error("Erro ao lidar com webhook:", error);
-
-  //   reply.type("text/xml").status(200).send(`
-  //       <Response>
-  //         <Message>Ocorreu um erro ao processar sua solicitação. Por favor, tente novamente mais tarde.</Message>
-  //       </Response>
-  //     `);
-  // }
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer EAAIeVdZAIBC4BO5R4ALgQ32OxWHgaEYkZC0gdz7Wy7S9TMAkOhg8TimKlAN8ZB8ST92Olj6t6IArCvISQYg0FKr7Liu7VOBjnwPbfWEjnJokv0XmTqUvI0Ngd73gdSjXSyoTC9f5Sxy0iwH4llk2iI0ywgzzWqswjH43jK14TAycZCVwmVXraeV1Q0MNWfZAuPOAgFNDT9koSbLOy1SUedj9oZAKMZD`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(messagePayload),
+  });
+  reply.status(200).send("EVENT_RECEIVED");
 };
