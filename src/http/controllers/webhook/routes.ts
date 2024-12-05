5ae5c00002421cd10830721dfefe887285ca1940
Pedro Arruda <arruda.pedro15@outlook.com>
2024-12-05 15:10:44 -0300

    feat: add verify token

--- src/http/controllers/webhook/routes.ts
+++ src/http/controllers/webhook/routes.ts
@@ -5,7 +5,7 @@ export const webhookRoutes = async (app: FastifyInstance) => {
   app.post("/webhook", handleWebhook);
 
   app.get("/webhook", (req: any, res) => {
-    const VERIFY_TOKEN = "seu_verify_token";
+    const VERIFY_TOKEN = "notion-assistant-api";
 
     const mode = req.query["hub.mode"];
     const token = req.query["hub.verify_token"];
