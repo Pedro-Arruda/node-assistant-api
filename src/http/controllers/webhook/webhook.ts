4f21f294893c24b51e6b823a9a3dbb953c335609
Pedro Arruda <arruda.pedro15@outlook.com>
2024-12-09 10:49:21 -0300

    fix: webhook add logs

--- src/http/controllers/webhook/webhook.ts
+++ src/http/controllers/webhook/webhook.ts
@@ -8,23 +8,21 @@ export const handleWebhook = async (req: any, reply: FastifyReply) => {
     accessToken: env.WHATSAPP_ACCESS_TOKEN,
   });
 
-  if (body.object === "whatsapp_business_account") {
-    body.entry.forEach((entry: any) => {
-      entry.changes.forEach((change: any) => {
-        const message = change.value.messages && change.value.messages[0];
-        if (
-          message &&
-          message.type === "interactive" &&
-          message.interactive.type === "button_reply"
-        ) {
-          const buttonId = message.interactive.button_reply.id;
-          console.log("buttonId", buttonId);
-        }
-      });
-    });
-  }
-
+  const message = req.body.entry[0].changes[0].value.messages[0];
   const sender = req.body.entry[0].changes[0].value.messages[0].from;
+
+  console.log("message", message);
+
+  if (
+    message &&
+    message.type === "interactive" &&
+    message.interactive.type === "button_reply"
+  ) {
+    const buttonId = message.interactive.button_reply.id;
+    console.log("buttonId", buttonId);
+    const response = await whatsappService.sendMessage(sender, buttonId);
+    console.log(response);
+  }
 
   const response = await whatsappService.sendNotionCategoriesButton(sender);
   console.log(response);
--- src/lib/whatsapp/index.ts
+++ src/lib/whatsapp/index.ts
@@ -14,10 +14,37 @@ export class WhatsappService {
     const response = await fetch(url, {
       method: "POST",
       headers: {
-        Authorization: `Bearer EAAIeVdZAIBC4BOZBYyFgkzWMj861ZCgAKAzGlqkg15EV4NjuKwX8SKFQVMJVVZAQwZAUL0kC8k2aGkGGmnisBBi5YAC0wMZAvHpAiUHZB4mNgpwut5KZC1xEPiEy88mHW31ZB2E5KAzw91snzxabi3nfkMeEn1ukgUmtDvCRmqif21ckKpuwlZAbVvErjnXXvfXbNGpAZDZD`,
+        Authorization: `Bearer ${this.accessToken}`,
         "Content-Type": "application/json",
       },
       body: JSON.stringify(messageTemplate),
+    });
+
+    if (!response.ok) {
+      throw new Error("Erro ao enviar template de button");
+    }
+
+    const data = await response.json();
+
+    return data;
+  }
+
+  async sendMessage(to: number, message: string) {
+    const url = `https://graph.facebook.com/v21.0/485600487973744/messages`;
+
+    const messageData = {
+      messaging_product: "whatsapp",
+      to,
+      text: { body: message },
+    };
+
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${this.accessToken}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(messageData),
     });
 
     if (!response.ok) {
